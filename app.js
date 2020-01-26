1c64a1cbeacbee99b5ea858e4a766f42696661a9
actionanand <meetanandns@gmail.com>
2020-01-26 20:01:51 +0530

    style

--- app.js
+++ app.js
@@ -1,3 +1,5 @@
+const path = require('path');
+
 const express = require('express');
 const bodyParser = require('body-parser');
 
@@ -5,12 +7,17 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+// const publicDir = path.join(__dirname, '/views');
+
+// app.use(express.static(publicDir));
+
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
+
 app.use((req, res, next) => {
-    res.status(404).send(`<h1>404</h1><h3>localhost:3001${req.url} is not found</h3>`);
+    res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 app.listen(3001, () => {
--- routes/admin.js
+++ routes/admin.js
@@ -1,9 +1,13 @@
+const path = require('path');
+
+const rootDir = require('../utils/path');
+
 const express = require('express');
 
 const router = express.Router();
 
 router.get('/add-product', (req, res, next) => {
-    res.send(`<form action="/admin/add-product" method="POST"><input type="text" name="title"><button type="submit">Send</button></form>`)
+    res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
 router.post('/add-product', (req, res, next) => {
