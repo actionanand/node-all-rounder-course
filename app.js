5ac8c3b22b7feaeae202592d6dc0ec7244301995
actionanand <meetanandns@gmail.com>
2020-01-26 16:13:28 +0530

    express added

--- app.js
+++ app.js
@@ -1,1 +1,11 @@
-console.log('hi node');
+const http = require('http');
+
+const express = require('express');
+
+const app = express();
+
+const server = http.createServer(app);
+
+server.listen(3001, () => {
+    console.log('Running at Port 3001');
+});
