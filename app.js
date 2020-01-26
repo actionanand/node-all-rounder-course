30aa07a767311e5f5dd679d2ec5ddf8f117b49bb
actionanand <meetanandns@gmail.com>
2020-01-26 18:30:21 +0530

    express routes

--- app.js
+++ app.js
@@ -1,11 +1,18 @@
-const http = require('http');
-
 const express = require('express');
+const bodyParser = require('body-parser');
 
 const app = express();
 
-const server = http.createServer(app);
+const adminRoutes = require('./routes/admin');
+const shopRoutes = require('./routes/shop');
 
-server.listen(3001, () => {
+app.use(bodyParser.urlencoded({extended: false}));
+app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+app.use((req, res, next) => {
+    res.status(404).send(`<h1>404</h1><h3>localhost:3001${req.url} is not found</h3>`);
+});
+
+app.listen(3001, () => {
     console.log('Running at Port 3001');
 });
