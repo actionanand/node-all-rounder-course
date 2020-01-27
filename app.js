f674b956db795148bcaa74904ebe47366557b4ee
actionanand <meetanandns@gmail.com>
2020-01-27 12:18:02 +0530

    working with handlebars

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const path = require('path');
 
 const express = require('express');
 const bodyParser = require('body-parser');
+const exphbs = require('express-handlebars');
 
 const app = express();
 
@@ -9,9 +10,13 @@ const {routes: adminRoutes} = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const publicDir = path.join(__dirname, 'public');
 const viewPath = path.join(__dirname, './templates/views');
+
+const layoutPath = path.join(__dirname, './templates/layouts');
 // const partialsPath = path.join(__dirname, '../templates/partials');
 
-app.set('view engine', 'pug');
+app.engine('handlebars', exphbs({ layoutsDir: layoutPath })); //file extension should be .handlebars it should match view engine name, if you wish you can take any name for view engine
+app.set('view engine', 'handlebars');
+// app.set('view engine', 'pug');
 app.set('views', viewPath);
 
 app.use(express.static(publicDir));
@@ -22,7 +27,7 @@ app.use(shopRoutes);
 app.use((req, res, next) => {
     // res.status(404).sendFile(path.join(__dirname, 'templates/static/404.html'));
     let url = `localhost:3001${req.url}`;
-    res.status(404).render('404', { title: 'Page Not Found', url })
+    res.status(404).render('404', { title: 'Page Not Found', url, layout: false })
 });
 
 app.listen(3001, () => {
